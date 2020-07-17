ef4ffc2911cf99fbd3ef7b344f996621674773d6
An Le <thaianle88@gmail.com>
2020-07-17 13:07:56 +0000

    update

---
+++ scripts/consume_dir.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+set -o xtrace
+
+pwd
+ls -lR
+
+cd my-repos
+
+ls -lR
+
+cat output/output_file.txt
+
---
+++ scripts/make_dir.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+set -o xtrace
+
+pwd
+
+output_dir = $pwd/output
+
+mkdir -p $output_dir
+
+echo "this is output content" >> $output_dir/output_file.txt
+
+ls -lR
+
