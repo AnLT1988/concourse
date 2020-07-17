3bb4863ac09046633a3398e5f29aa19c2e4023a2
An Le <thaianle88@gmail.com>
2020-07-17 13:30:24 +0000

    update

--- scripts/make_dir.sh
+++ scripts/make_dir.sh
@@ -8,7 +8,7 @@ output_dir = $pwd/output
 
 mkdir -p $output_dir
 
-echo "this is output content" >> $output_dir/output_file.txt
+echo "this is output content" > $output_dir/output_file.txt
 
 ls -lR
 
